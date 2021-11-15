b18ce8ecef79a80a22d3bafddcfa1096220aed27
OlPeep <evilmall@mail.ru>
2021-11-15 14:20:31 +0300

    fixed item bid adding

--- auctions/admin.py
+++ auctions/admin.py
@@ -4,12 +4,17 @@ from .models import User, Category, Auction, Bid, Comment, Watchlist
 
 admin.site.register(User)
 admin.site.register(Category)
-#admin.site.register(Auction)
-admin.site.register(Bid)
-admin.site.register(Comment)
 admin.site.register(Watchlist)
 
 class AuctionAdmin(admin.ModelAdmin):
     list_display = ('user', 'title', 'startbid', 'active', 'buyer')
 
+class BidAdmin(admin.ModelAdmin):
+    list_display = ('user', 'item', 'price')
+
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('user', 'item', 'texty')
+
 admin.site.register(Auction, AuctionAdmin)
+admin.site.register(Bid, BidAdmin)
+admin.site.register(Comment, CommentAdmin)
--- auctions/views.py
+++ auctions/views.py
@@ -22,13 +22,9 @@ def index(request):
 
 def login_view(request):
     if request.method == "POST":
-
-        # Attempt to sign user in
         username = request.POST["username"]
         password = request.POST["password"]
         user = authenticate(request, username=username, password=password)
-
-        # Check if authentication successful
         if user is not None:
             login(request, user)
             return HttpResponseRedirect(reverse("index"))
@@ -49,16 +45,12 @@ def register(request):
     if request.method == "POST":
         username = request.POST["username"]
         email = request.POST["email"]
-
-        # Ensure password matches confirmation
         password = request.POST["password"]
         confirmation = request.POST["confirmation"]
         if password != confirmation:
             return render(request, "auctions/register.html", {
                 "message": "Passwords must match."
             })
-
-        # Attempt to create new user
         try:
             user = User.objects.create_user(username, email, password)
             user.save()
@@ -74,11 +66,6 @@ def register(request):
 @login_required
 def new(request):
     categories = Category.objects.all()
-    users = User.objects.all()
-    for user in users:
-        if user.is_authenticated:
-            idd = user.id
-            owner = User.objects.get(pk=idd)
     if request.method == "POST":
         form = AuctionForm(request.POST, request.FILES)
         if form.is_valid():
@@ -86,14 +73,14 @@ def new(request):
             description = form.cleaned_data["description"]
             start = form.cleaned_data["startbid"]
             image = request.FILES['file']
-            auction = Auction(user=owner, title=title, description=description, start=start, image=image)
+            auction = Auction(user=request.user, title=title, description=description, start=start, image=image)
             auction.save()
             listiks = request.POST.getlist('category')
             for listik in listiks:
                 category_id = int(listik)
                 category = Category.objects.get(pk=category_id)
                 auction.category.add(category)
-            return HttpResponseRedirect(reverse("index"))
+            return HttpResponseRedirect(reverse("listing", args=(auction.id,)))
         else:
             return render(request, "auctions/new.html", {
                 "form": form
@@ -108,43 +95,37 @@ def new(request):
 def listing(request, num):
     listing = Auction.objects.get(pk=num)
     group = listing.category.all()
-    users = User.objects.all()
-    for user in users:
-        if user.is_authenticated:
-            name = user.username
-            author = User.objects.get(pk=user.id)
-            if request.method == "POST":
-                comment = request.POST["comment"]
-                create = Comment(texty=comment, user=author, item=listing)
-                create.save()
-                return HttpResponseRedirect(reverse("listing", args=(num,)))
-            else:
-                comments = Comment.objects.filter(item=listing)
-                added = Watchlist.objects.filter(user=name, item=num) 
-                return render(request, "auctions/listing.html", {
-                    "added": added,
-                    "listing": listing,
-                    "group": group,
-                    "comments": comments
-                    })
+    if request.method == "POST":
+        comment = request.POST["comment"]
+        create = Comment(texty=comment, user=request.user, item=listing)
+        create.save()
+        return HttpResponseRedirect(reverse("listing", args=(num,)))
+    else:
+        comments = Comment.objects.filter(item=listing)
+        bids = Bid.objects.filter(item=listing)
+        added = Watchlist.objects.filter(user=request.user.username, item=num) 
+        print(listing.buyer, request.user.username)
+        return render(request, "auctions/listing.html", {
+            "added": added,
+            "listing": listing,
+            "group": group,
+            "comments": comments,
+            "bids": bids
+            })
 
 @login_required
 def wish(request, num):
     listing = Auction.objects.get(pk=num)
     group = listing.category.all()
     n = listing.user.id
-    users = User.objects.exclude(pk=n)
-    for user in users:
-        if user.is_authenticated:
-            name = user.username
-            wish = Watchlist.objects.filter(user=name, item=num)
-            if wish:
-                wish.delete()
-                return HttpResponseRedirect(reverse("listing", args=(num,)))
-            else:
-                wish = Watchlist(user=name, item=num)
-                wish.save()
-                return HttpResponseRedirect(reverse("listing", args=(num,)))
+    wish = Watchlist.objects.filter(user=request.user.username, item=num)
+    if wish:
+        wish.delete()
+        return HttpResponseRedirect(reverse("listing", args=(num,)))
+    else:
+        wish = Watchlist(user=request.user.username, item=num)
+        wish.save()
+        return HttpResponseRedirect(reverse("listing", args=(num,)))
 
 @login_required
 def bid(request, num):
@@ -152,36 +133,51 @@ def bid(request, num):
         listing = Auction.objects.get(pk=num)
         group = listing.category.all()
         n = listing.user.id
-        users = User.objects.exclude(pk=n)
-        for user in users:
-            if user.is_authenticated:
-                name = user.username
-                bids = Bid.objects.filter(item=listing)
-                bid = bids.order_by('-price').first()
-                mybid = float(request.POST["bid"])
-                if bid is None:
-                    maxprice = listing.start
-                else:
-                    maxprice = float(bid.price)
-                    if maxprice > mybid:
-                        listing.startbid = maxprice
-                        added = Watchlist.objects.filter(user=name, item=num)
-                        comments = Comment.objects.filter(item=listing)
-                        return render(request, "auctions/listing.html", {
-                            "added": added,
-                            "listing": listing,
-                            "group": group,
-                            "comments": comments,
-                            "mes": "Your bid should be greater than previous"
-                            })
-                    else:
-                        listing.startbid = mybid
-                buyer = User.objects.get(pk=user.id)
-                ist = Bid(price=mybid, user=buyer, item=listing)
-                ist.save()
-                listing.buyer = user.username
-                listing.save()
-                return HttpResponseRedirect(reverse("listing", args=(num,)))
+        added = Watchlist.objects.filter(user=request.user.username, item=num)
+        comments = Comment.objects.filter(item=listing)
+        bids = Bid.objects.filter(item=listing)
+        bid = bids.order_by('-price').first()
+        mybid = float(request.POST["bid"])
+        if bid is not None:
+            maxprice = float(bid.price)
+            if maxprice >= mybid:
+                listing.startbid = maxprice
+                return render(request, "auctions/listing.html", {
+                    "added": added,
+                    "listing": listing,
+                    "group": group,
+                    "comments": comments,
+                    "bids": bids,
+                    "mes": "Your bid should be greater than previous"
+                    })
+            else:
+                listing.startbid = mybid
+        else:
+            if mybid >= listing.start:
+                listing.startbid = mybid
+            else:
+                return render(request, "auctions/listing.html", {
+                    "added": added,
+                    "listing": listing,
+                    "group": group,
+                    "comments": comments,
+                    "bids": bids,
+                    "mes": "Your bid should be greater than previous"
+                    })
+        buyer = User.objects.get(pk=request.user.id)
+        ist = Bid(price=mybid, user=buyer, item=listing)
+        ist.save()
+        listing.buyer = request.user.username
+        listing.save()
+        print(listing.buyer)
+        return render(request, "auctions/listing.html", {
+            "added": added,
+            "listing": listing,
+            "group": group,
+            "comments": comments,
+            "bids": bids,
+            "mes": "Your bid was saved"
+            })
 
 @login_required
 def close(request, num):
@@ -195,7 +191,7 @@ def close(request, num):
 
 @login_required
 def watchlist(request, name):
-    here = Watchlist.objects.filter(user=name)
+    here = Watchlist.objects.filter(user=request.user.username)
     auctions = []
     for i in here:
         j = i.item
